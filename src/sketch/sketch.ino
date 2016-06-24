d93b6b3165ac47d6b990e054618989c52663819e
Thiago Salles <tsmlima@gmail.com>
2016-06-24 08:59:35 -0300

    Removing potentiometer things.

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -1,23 +1,13 @@
 /*
-	Speaking Robot
+  Speaking Robot
 */
 
 #include <MorseTalk.h>
-
-// Output numbers for the amplifier
-#define MIN_ANALOG_OUTPUT 127
-#define MAX_ANALOG_OUTPUT 255
 
 // Digital Pins
 #define RIGHT_SPEAKER_PIN 2
 #define MORSE_LED_PIN 4
 #define TILT_LED_PIN 3
-// Analog Pins
-#define AMPLIFIER_PIN 7
-#define POT_PIN 6
-
-// Stores the potentiometer value
-int potVal = 0;
 
 MorseTalk morseTalk(RIGHT_SPEAKER_PIN, MORSE_LED_PIN, TILT_LED_PIN);
 
@@ -29,22 +19,6 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-
-  // Reading the potentiometer
-  potVal = analogRead(POT_PIN);
-  //Serial.print("Value to use as amplifier input (Volume): ");
-  //Serial.println(outputBasedOnPot(potVal));
-
   morseTalk.process();
-  delay(millis()/100); // a cada 1s serão 10ms de delay. 10s para 100ms que faz o tilt
-}
-
-float potMultiplier(int potVal) {
-  return potVal / 1023.0;
-}
-
-int outputBasedOnPot(int potVal) {
-  int outputDiff = MAX_ANALOG_OUTPUT - MIN_ANALOG_OUTPUT;
-  return MIN_ANALOG_OUTPUT + outputDiff * potMultiplier(potVal);
 }
 
